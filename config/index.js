c1f4d9cd03e4274d6835e96ce8469e998f7b0ac5
DotZero27 <dotzero27@gmail.com>
2024-05-29 10:56:11 +0530

    initial commit

---
+++ config/index.js
@@ -0,0 +1,22 @@
+export const PROJECTS = [
+  {
+    title: "Research Publications",
+    description: "Manage the research publications of the department",
+    url: "http://10.7.1.178:3000",
+  },
+  {
+    title: "Workload Management",
+    description: "Manage the workload hours of the faculty.",
+    url: "http://10.7.1.178:5000/",
+  },
+  {
+    title: "Result Analysis",
+    description: "Analyze the results of the students of the department",
+    url: "http://10.7.1.178:8000/",
+  },
+  {
+    title: "NPTEL Management System",
+    description: "Manage the NPTEL exam records of the department",
+    url: "http://10.7.1.178:8081/",
+  },
+];
